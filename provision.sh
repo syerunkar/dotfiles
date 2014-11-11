78a57a49c66aaff285de7cfbdf115b7985c4e940
Sami Yerunkar <samiyerunkar@gmail.com>
2014-11-11 04:15:57 -0800

    Added a more descriptive provision file header

--- provision.sh
+++ provision.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# ./provision.sh <OPERATING_SYSTEM>
+# use as such:
+# ./provision.sh mac
 ln -s .tmux.conf ~
 ln -s .bashrc ~
 ln -s .zshrc ~
@@ -7,26 +10,27 @@ ln -s .vimrc ~
 # Install Vundle, assumes git is installed
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-if [ $3 == "ubuntu" ]; then
+if [ $2 == "ubuntu" ]; then
   sudo apt-get update;
-  sudo apt-get install -y curl vim nodejs npm docker tmux
+  sudo apt-get install -y curl vim nodejs npm docker tmux zsh
 fi
 
-if [ $3 == "centos" ]; then
+if [ $2 == "centos" ]; then
   sudo yum update;
   # Nodejs startup script
   curl -sL https://rpm.nodesource.com/setup | bash -
-  sudo yum install -y curl vim nodejs npm docker tmux
+  sudo yum install -y curl vim nodejs npm docker tmux zsh
 fi
 
-if [ $3 == "mac" ]; then
+if [ $2 == "mac" ]; then
   # Install homebrew
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install tmux zsh node
 fi
 
 # Install omz and version managers. Assumes curl is installed.
 # I trust these sources. Do you?
-if [ $2 == "true" ]; then
+if [ $2 != "" ]; then
   # Install oh-my-zsh, assumes zsh is installed
   curl -L http://install.ohmyz.sh | sh
 
